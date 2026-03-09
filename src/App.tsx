3618fbcfab7abe5e3eb95d049ee9dad2111110dc
H27films <hamzar.128@gmail.com>
2026-03-09 11:05:40 +0800

    Add /landing2 route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
+import Landing2 from "./pages/Landing2";
 import Index from "./pages/Index";
 import Stock from "./pages/Stock";
 import StockNurYadi from "./pages/StockNurYadi";
@@ -20,6 +21,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/landing2" element={<Landing2 />} />
           <Route path="/prices" element={<Index />} />
           <Route path="/stock" element={<Stock />} />
           <Route path="/stocknuryadi" element={<StockNurYadi />} />
